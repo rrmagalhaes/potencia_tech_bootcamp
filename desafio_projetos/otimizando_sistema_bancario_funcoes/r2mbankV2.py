84273ade6186f7d746948c4463e9cb4d9eb19c6a
rrmagalhaes <rrmagalhaes@hotmail.com.br>
2023-08-01 20:33:41 -0300

    refactor: Cria funcões para saque, depósito e extrato.

--- desafio_projetos/otimizando_sistema_bancario_funcoes/r2mbankV2.py
+++ desafio_projetos/otimizando_sistema_bancario_funcoes/r2mbankV2.py
@@ -19,6 +19,44 @@ numero_saques = 0
 LIMITE_QTD_SAQUE = 3
 LIMITE_VALOR_SAQUE = 500
 
+def depositar(saldo, valor, extrato, /,):
+    if valor <= 0:
+            print("OPERAÇÃO INVÁLIDA")
+            print("O valor de depósito precisar ser positivo!")
+    else:
+        saldo += valor
+        extrato = extrato + f"DEPÓSITO => + R$ {valor:.2f}\n"
+        print("Depósito realizado com sucesso!")
+        return saldo, extrato
+
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    if numero_saques == limite_saques:
+            print(f"Saque indisponível - Seu limite diário de saques por dia é apenas {limite_saques} saques.")  
+    elif valor > limite:
+            print(f"Saque indisponível - Seu valor limite por saque é de R$ {limite:.2f}")
+    else:
+        if valor <= saldo:
+            numero_saques += 1
+            saldo -= valor
+            extrato = extrato + f"SAQUE => - R$ {valor:.2f}\n"
+            print("Saque realizado com sucesso!")
+            return saldo, extrato
+        else:
+            print("Saldo insuficiente!")
+
+    
+#   return saldo, extrato
+
+
+def ver_extrato(saldo, /, *, extrato):
+    print("====================== EXTRATO ==============================")
+    print(f"{extrato}")
+    print(f"SALDO ATUAL => R$ {saldo:.2f}")
+    print("=============================================================")
+    input("Pressione ENTER voltar ao menu inicial!")
+
+
 while True:
     print(start)
     choice = str(input("Favor selecione a opção desejada: "))
@@ -31,43 +69,26 @@ while True:
         print(exit_msg)
         break
 
+
     # DEPOSITAR
     elif choice == "1":
         value = float(input("Digite o valor do despósito a realizar: R$ ").replace(",","."))
-        if value <= 0:
-            print("OPERAÇÃO INVÁLIDA")
-            print("O valor de depósito precisar ser positivo!")
-            continue
-        else:
-            saldo += value
-            extrato = extrato + f"\nDEPÓSITO => + R$ {value:.2f}"
-            print("Depósito realizado com sucesso!")
+        deposito = depositar(saldo, value, extrato)
+        if deposito != None:
+            saldo = deposito[0]
+            extrato = deposito[1]
+
 
     # SACAR
     elif choice == "2":
         value = float(input("Digite o valor do saque a realizar: R$ ").replace(",","."))
-        if numero_saques == LIMITE_QTD_SAQUE:
-            print(f"Saque indisponível - Seu limite diário de saques por dia é apenas {LIMITE_QTD_SAQUE} saques.")
-            continue
-        elif value > LIMITE_VALOR_SAQUE:
-            print(f"Saque indisponível - Seu valor limite por saque é de R$ {LIMITE_VALOR_SAQUE:.2f}")
-        else:
-            if value <= saldo:
-                numero_saques += 1
-                saldo -= value
-                extrato = extrato + f"\nSAQUE => - R$ {value:.2f}"
-                print("Saque realizado com sucesso!")
-            else:
-                print("O valor solicitado é maior que o saldo disponível em conta.")
-                continue
-
-
+        saque = sacar(saldo=saldo, valor=value, extrato=extrato, limite=LIMITE_VALOR_SAQUE, numero_saques=numero_saques, limite_saques=LIMITE_QTD_SAQUE)
+        if saque != None:
+            saldo = saque[0]
+            extrato = saque[1]
+            numero_saques += 1
+    
 
     # EXTRATO
     elif choice == "3":
-        print(f'''
-        {extrato}
-
-        SALDO ATUAL => R$ {saldo:.2f}
-        ''')
-        input("Pressione ENTER voltar ao menu inicial!")
+        ver_extrato(saldo, extrato=extrato)
